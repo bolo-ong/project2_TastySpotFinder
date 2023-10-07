0ad78c854246821eb7afb0c1d3571d0c55c0721b
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-07 23:57:00 +0900

    [FEAT] 경량화 & disabled 상태 추가
    불필요한 함수를 상수로 변경,
    disabled상태를 추가하면서 hover에도 예외처리 추가

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,26 +1,43 @@
 import styled from "@emotion/styled";
 
 interface Props {
+  bgColor?: string;
+  color?: string;
+  fontSize?: string | number;
+  fontWeight?: string | number;
   width?: string | number;
   height?: string | number;
-  border?: string | number;
-  disabled?: boolean;
+  borderColor?: string;
   rounded?: boolean;
-  color: string;
 }
 
 export const Button = styled.button<Props>`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  font-size: ${({ fontSize, theme }) =>
+    fontSize ? theme.pxToRem(parseInt(`${fontSize}`)) : "1rem"};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  color: ${({ color, theme }) => (color ? `${color}` : theme.colors.black)};
+
   width: ${({ width, theme }) =>
     width ? theme.pxToRem(parseInt(`${width}`)) : "100%"};
   height: ${({ height, theme }) =>
     height ? theme.pxToRem(parseInt(`${height}`)) : "100%"};
-  border: ${({ border, theme }) =>
-    border ? `${theme.pxToRem(parseInt(`${border}`))} solid #d3d3d3` : "none"};
-  border-radius: ${({ rounded, theme }) =>
-    rounded ? theme.pxToRem(100) : theme.pxToRem(4)};
-  background-color: ${({ disabled, color, theme }) =>
-    disabled ? theme.colors.gray : color};
+  background-color: ${({ bgColor, theme }) =>
+    bgColor ? `${bgColor}` : theme.colors.main};
+  border-radius: ${({ rounded }) => (rounded ? "100%" : "0.25rem")};
+  border: ${({ borderColor }) =>
+    borderColor ? `0.0625rem solid ${borderColor}` : "none"};
+
+  &:hover:not(:disabled) {
+    transition: 0.2s ease-in-out;
+    filter: brightness(0.9);
+  }
+  &:disabled {
+    cursor: auto;
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.gray};
+  }
 `;
