a045e7ad689d4589fc95bb776363abdfcfc5f276
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-08 23:32:34 +0900

    [FEAT] create SocialLogInButton Component
    1.map메서드를 사용할 시 오히려 코드가 지저분해지고 길어지며,
    구조를 확인하고 platforms배열로 다시 찾아가서 수정해야 하는 번거로움이 생김 const platforms = [
      { platform: "naver", text: "네이버 로그인" },
      { platform: "kakao", text: "카카오 로그인" },
      { platform: "google", text: "구글 로그인" },
    ];
    {platforms.map(({ platform, text }) => (
            <SocialLogInButton
              key={platform}
              platform={platform}
            >
              {text}
            </SocialLogInButton>
    
    2.SocialLongInButtonGroup 컴포넌트로 묶는다면, 구조가 복잡해지고, 플랫폼을 추가하려면, SocialLongInButtonGroup컴포넌트와 SocialLongInButton컴포넌트 모두 확인하고 수정해야하는 번거로움이 발생,
    하지만 그에비해 수정할일이 적은 컴포넌트 이기 때문에 반복해서 작성하기로함

---
+++ client/src/components/SocialLogInButton.tsx
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { Image } from "./index";
+
+export const SocialLogInButton = ({ children, platform }: Props) => {
+  return (
+    <Link to={`${process.env.REACT_APP_SERVER_URL}/api/auth/${platform}`}>
+      <StyledSocialLogInButton platform={platform}>
+        <ImageWrapper>
+          <Image name={`logo_${platform}`} extension="svg" />
+        </ImageWrapper>
+        {children}
+      </StyledSocialLogInButton>
+    </Link>
+  );
+};
+
+interface Props {
+  platform: "kakao" | "google" | "naver";
+  children: React.ReactNode;
+}
+
+const StyledSocialLogInButton = styled.button<Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  min-width: 18.75rem;
+  height: 3.25rem;
+  font-size: 1.125rem;
+  font-weight: 600;
+  border-radius: 0.25rem;
+
+  &:hover {
+    transition: 0.2s ease-in-out;
+    filter: brightness(0.9);
+  }
+  ${({ platform }) => {
+    switch (platform) {
+      case "kakao":
+        return `
+            background-color: #ffdf00;
+            color: #3B1D1B;
+          `;
+      case "naver":
+        return `
+            background-color: #00BC3D;
+            color: #E1F7E8;
+          `;
+      case "google":
+        return `
+            background-color: #ffffff;
+            border: 0.0625rem solid #D3D3D3;
+          `;
+    }
+  }};
+`;
+
+const ImageWrapper = styled.div`
+  position: absolute;
+  left: 1.125rem;
+`;
