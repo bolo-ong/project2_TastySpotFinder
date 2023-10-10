c056c59ad0e88a3400760b47119d70cbd698d0c4
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-10 15:34:17 +0900

    [Refactor] img태그를 styled Component로 변경

--- client/src/components/common/Image.tsx
+++ client/src/components/common/Image.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-interface Props {
-  width?: string | number;
-  height?: string | number;
-  name: string;
-  extension: string;
-}
-
 export const Image = ({ width, height, name, extension }: Props) => {
   return (
-    <Wrapper width={width} height={height}>
-      <img
-        src={`/images/${name}.${extension}`}
-        alt={name}
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
-      />
-    </Wrapper>
+    <ImageWrapper width={width} height={height}>
+      <StyledImage src={`/images/${name}.${extension}`} alt={name} />
+    </ImageWrapper>
   );
 };
 
-const Wrapper = styled.div<{
+interface Props {
+  name: string;
+  extension: string;
   width?: string | number;
   height?: string | number;
-}>`
+}
+
+type ImageWrapperProps = Pick<Props, "width" | "height">;
+const ImageWrapper = styled.div<ImageWrapperProps>`
   width: ${({ width, theme }) =>
     width ? theme.pxToRem(parseInt(`${width}`)) : "100%"};
   height: ${({ height, theme }) =>
     height ? theme.pxToRem(parseInt(`${height}`)) : "100%"};
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
