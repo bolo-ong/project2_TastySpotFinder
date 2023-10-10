a910162d7e756ad25b45cbb0180b905ca2a0bf35
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-10 15:47:26 +0900

    [FEAT] add SocialLogInButton

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,7 +1,9 @@
+export { Image } from "./common/Image";
+export { Text } from "./common/Text";
+export { Button } from "./common/Button";
+export { Avatar } from "./common/Avatar";
+
 export { Navbar } from "./Navbar";
 export { Navgation } from "./Navgation";
 export { NaverMap } from "./NaverMap";
-export { LogIn } from "./LogIn";
-//common
-export { Icon } from "./common/Icon";
-export { Text } from "./common/Text";
+export { SocialLogInButton } from "./SocialLogInButton";
