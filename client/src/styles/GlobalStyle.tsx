38559e9da58b7158862d9d02da1288a812be1ef7
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-10 15:41:53 +0900

    [FEAT] update GlobalStyle

--- client/src/styles/GlobalStyle.tsx
+++ client/src/styles/GlobalStyle.tsx
@@ -21,39 +21,46 @@ export const GlobalStyle = () => {
             BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
             "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
             "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-
-          //피그마 글로버 스타일
-
-          /* font sizes */
-          --font-size-sm: 14px;
-          --font-size-md: 16px;
-          --font-size-lg: 18px;
-          /* Colors */
-          --color-white: #fff;
-          --color-black: #333;
-          --color-gray: #e9ebed;
-          --color-darkgray: #9b9ea1;
-          --color-main: #79482d;
-
-          /* Gaps */
-          --gap-xl: 20px;
-          --gap-base: 16px;
-          --gap-5xs: 8px;
-          /* Paddings */
-          --padding-9xs: 4px;
-          --padding-10xs: 3px;
-          --padding-8xs: 5px;
-          --padding-base: 16px;
-          --padding-5xs: 8px;
-          --padding-xs: 12px;
-          /* Border radiuses */
-          --br-81xl: 100px;
-          --br-xs: 12px;
         }
-
+        #root {
+          height: 100%;
+        }
+        html,
+        body {
+          height: 100%;
+        }
         a {
+          height: 100%;
+          color: inherit;
           text-decoration: none;
-          color: var(--color-black);
+        }
+        button {
+          padding: 0;
+          text-align: inherit;
+          font: inherit;
+          letter-spacing: inherit;
+          color: inherit;
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+        input {
+          padding: 0;
+          font: inherit;
+          letter-spacing: inherit;
+          color: inherit;
+          background: none;
+          border: none;
+        }
+        textarea {
+          padding: 0;
+          vertical-align: middle;
+          font: inherit;
+          letter-spacing: inherit;
+          color: inherit;
+          background: none;
+          border: none;
+          resize: none;
         }
       `}
     />
