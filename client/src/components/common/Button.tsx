a5e1089cdfbb2c26b00b8b9e099639c5a97dc031
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-25 21:11:09 +0900

    [FEAT] create Button component

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -2,14 +2,15 @@ import styled from "@emotion/styled";
 import { theme } from "styles/theme";
 
 export const Button = ({
-  variant = "filled",
-  size = "md",
+  variant,
+  size,
   children,
   startIcon,
   endIcon,
+  disabled,
 }: Props) => {
   return (
-    <StyledButton size={size} variant={variant}>
+    <StyledButton size={size} variant={variant} disabled={disabled}>
       {startIcon}
       {children}
       {endIcon}
@@ -18,28 +19,74 @@ export const Button = ({
 };
 
 interface Props {
-  variant: "filled" | "outlined" | "warning";
+  variant: "filled" | "warning" | "outlined";
   size: "lg" | "md" | "sm";
   children?: React.ReactNode;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
+  disabled?: boolean;
 }
 
-const variants = {
-  filled: {
-    color: theme.colors.white,
-    backgroundColor: theme.colors.main,
-  },
-  outlined: {
-    color: theme.colors.main,
-    backgroundColor: theme.colors.white,
-  },
-  warning: {
-    color: theme.colors.white,
-    backgroundColor: theme.colors.warning,
-  },
+Button.defaultProps = {
+  variant: "filled",
+  size: "md",
 };
 
+const StyledButton = styled.button<Props>`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  gap: 0.5rem;
+  font-weight: 500;
+  border-radius: 0.75rem;
+
+  padding: ${({ variant }) =>
+    variant === "outlined" ? "0 .9375rem" : "0 1rem"};
+  border: ${({ variant, theme }) =>
+    variant === "outlined" ? `.0625rem solid ${theme.colors.gray[3]}` : ""};
+
+  font-size: ${({ size }) => sizes[size].fontSize};
+  height: ${({ size }) => sizes[size].height};
+
+  color: ${({ variant }) => variants[variant].color};
+  background-color: ${({ variant }) => variants[variant].backgroundColor};
+
+  > svg {
+    width: ${({ size }) => sizes[size].fontSize};
+    height: ${({ size }) => sizes[size].fontSize};
+  }
+
+  &:hover:not(:disabled) {
+    background-color: ${({ variant }) =>
+      variants[variant].hoverBackgroundColor};
+    box-shadow: 0 0 0.125rem #202020;
+  }
+
+  &:active:not(:disabled) {
+    background-color: ${({ variant }) =>
+      variant === "outlined"
+        ? variants[variant].activeBackgroundColor
+        : variants[variant].backgroundColor};
+    padding: 0 0.875rem;
+    border: 0.125rem solid
+      ${({ variant }) => variants[variant].activeBorderColor};
+  }
+
+  &:disabled {
+    cursor: auto;
+    color: ${({ variant }) => variants[variant].disabledColor};
+    background-color: ${({ variant }) =>
+      variant === "outlined"
+        ? variants[variant].backgroundColor
+        : variants[variant].disabledBackgroundColor};
+    border: ${({ variant }) =>
+      variant === "outlined" ? `.0625rem solid ${theme.colors.gray[1]}` : ""};
+  }
+`;
+
+//constants
 const sizes = {
   lg: {
     fontSize: "1rem",
@@ -55,38 +102,42 @@ const sizes = {
   },
 };
 
-const StyledButton = styled.button<Props>`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-
-  gap: 0.5rem;
-  padding: 0 1rem;
-  font-weight: 500;
-  border-radius: 12px;
-
-  font-size: ${({ size }) => sizes[size].fontSize};
-  height: ${({ size }) => sizes[size].height};
-
-  /* border */
-  color: ${({ variant }) => variants[variant].color};
-  background-color: ${({ variant }) => variants[variant].backgroundColor};
-
-  > svg {
-    width: ${({ size }) => sizes[size].fontSize};
-    height: ${({ size }) => sizes[size].fontSize};
-    fill: ${({ variant }) => variants[variant].color};
-  }
-
-  &:hover:not(:disabled) {
-  }
-
-  &:focus:not(:disabled) {
-  }
-  &:active:not(:disabled) {
-  }
-
-  &:disabled {
-  }
-`;
+const variants = {
+  filled: {
+    //default
+    color: theme.colors.white,
+    backgroundColor: theme.colors.main[5],
+    //hover & focus
+    hoverBackgroundColor: theme.colors.main[7],
+    //active
+    activeBorderColor: theme.colors.main[7],
+    //disabled
+    disabledColor: theme.colors.gray[5],
+    disabledBackgroundColor: theme.colors.gray[1],
+  },
+  warning: {
+    //default
+    color: theme.colors.white,
+    backgroundColor: theme.colors.warning[5],
+    //hover & focus
+    hoverBackgroundColor: theme.colors.warning[7],
+    //active
+    activeBorderColor: theme.colors.warning[7],
+    //disabled
+    disabledColor: theme.colors.warning[1],
+    disabledBackgroundColor: theme.colors.warning[0],
+  },
+  outlined: {
+    //default
+    color: theme.colors.main[5],
+    backgroundColor: theme.colors.white,
+    //hover & focus
+    hoverBackgroundColor: theme.colors.main[0],
+    //active
+    activeBackgroundColor: theme.colors.main[0],
+    activeBorderColor: theme.colors.main[1],
+    //disabled
+    disabledColor: theme.colors.gray[4],
+    disabledBorderColor: theme.colors.gray[1],
+  },
+};
