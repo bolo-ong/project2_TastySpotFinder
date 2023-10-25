5928a249bf34b683a95e3dda827f703133f505b1
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-10-25 21:09:53 +0900

    [STYLE] white컬러코드 변경

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -3,7 +3,7 @@ import { Theme } from "@emotion/react";
 export const theme: Theme = {
   //color: [50,100,200,300,400,500,600,700,800,900]
   colors: {
-    white: "#ffffff",
+    white: "#fcfcfc",
     black: "#333333",
     main: [
       "#F2EDEA",
