ffc66521f6f54629d8a87ed1252d4b003bf76193
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-01 21:46:08 +0900

    [FEAT] set RecoilRoot

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { theme, GlobalStyle } from "./styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Main, Board, LogIn } from "./pages";
+import { Main, Board, LogIn, Posting } from "./pages";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,20 +17,23 @@ const queryClient = new QueryClient({
 const App = () => (
   <>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
 
-        <Router>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/board" element={<Board />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="*" element={<div>404</div>} />
-          </Routes>
-        </Router>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/board" element={<Board />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/posting" element={<Posting />} />
+              <Route path="*" element={<div>404</div>} />
+            </Routes>
+          </Router>
 
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   </>
 );
