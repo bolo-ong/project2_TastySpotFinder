eddf09b5478553c70eae853ac661d75ddc7212f8
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-05 21:23:25 +0900

    [FEAT] delete Navigation compoenet
    Navbar componenet에 병합

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,6 +9,17 @@ export const Navbar = () => {
   const { userData } = useGetUserDataQuery();
   console.log(userData);
 
+  const menuItems = [
+    { component: <Link to="/">저장한 리스트</Link> },
+    { component: <Link to="/board">저장한 맛집</Link> },
+    { component: <Link to="#">나의 맛집</Link> },
+    {
+      component: (
+        <button onClick={() => userLogOut(userData.provider)}>로그아웃</button>
+      ),
+    },
+  ];
+
   return (
     <Container>
       <Image
@@ -31,20 +42,9 @@ export const Navbar = () => {
                 <Avatar size="36" src={userData.profile_image} hoverable />
               }
             >
-              <MenuItem>
-                <Link to="/">저장한 리스트</Link>
-              </MenuItem>
-              <MenuItem>
-                <Link to="/board">저장한 맛집</Link>
-              </MenuItem>
-              <MenuItem>
-                <Link to="#">나의 맛집</Link>
-              </MenuItem>
-              <MenuItem>
-                <button onClick={() => userLogOut(userData.provider)}>
-                  로그아웃
-                </button>
-              </MenuItem>
+              {menuItems.map((menuItem, idx) => (
+                <MenuItem key={idx}>{menuItem.component}</MenuItem>
+              ))}
             </DropdownMenu>
 
             <Button as={Link} to="/posting">
