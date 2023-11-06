3f228d4a4aea6ba0346fca1564d30d5dfe0a65dc
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-06 23:31:40 +0900

    [FEAT] update Image component
    add hoverable
    remove object-fit:cover

--- client/src/components/common/Image.tsx
+++ client/src/components/common/Image.tsx
@@ -16,20 +16,40 @@ export interface Props {
   extension: string;
   width?: string | number;
   height?: string | number;
+  hoverable?: boolean;
   as?: React.ElementType;
   to?: string;
 }
 
-type ImageWrapperProps = Pick<Props, "width" | "height">;
+type ImageWrapperProps = Pick<Props, "width" | "height" | "hoverable">;
 const ImageWrapper = styled.div<ImageWrapperProps>`
   width: ${({ width, theme }) =>
     width ? theme.pxToRem(parseInt(`${width}`)) : "100%"};
   height: ${({ height, theme }) =>
     height ? theme.pxToRem(parseInt(`${height}`)) : "100%"};
+
+  ${({ hoverable }) =>
+    hoverable &&
+    `
+    &::after {
+      content: "";
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 100%;
+      background-color: #220c0c;
+      opacity: 0;
+    }
+    &:hover::after {
+      opacity: 0.2;
+    }
+    `}
 `;
 
 const StyledImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
 `;
