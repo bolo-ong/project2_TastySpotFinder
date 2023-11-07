f5d761ee881da7e9e5ecfed2a7a0de661e94c51a
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-08 00:35:11 +0900

    [FEAT] create SearchBar component

---
+++ client/src/components/common/SearchBar.tsx
@@ -0,0 +1,98 @@
+import React, { FormEvent, ChangeEvent, useState } from "react";
+import styled from "@emotion/styled";
+import { Image } from "components";
+import { theme } from "styles/theme";
+
+export const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    //inputValue.replace(/ /g, '') === '' 일 땐 submit안되게
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setInputValue("");
+  };
+
+  return (
+    <Container>
+      <form>
+        <InputContainer>
+          <ImageWrapper position="left">
+            <Image name="icon_search" extension="svg" height={16} />
+          </ImageWrapper>
+          <StyledInput
+            value={inputValue}
+            type="text"
+            placeholder={`placeholderText`}
+            onSubmit={handleSubmit}
+            onChange={handleChange}
+          />
+          {inputValue && (
+            <ImageWrapper position="right" onClick={handleClick}>
+              <ClearIconWrapper>
+                <Image
+                  name="icon_clear"
+                  extension="svg"
+                  height={16}
+                  hoverable
+                />
+              </ClearIconWrapper>
+            </ImageWrapper>
+          )}
+        </InputContainer>
+      </form>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 35rem;
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  border-radius: 6.25rem;
+  font-weight: 700;
+  padding: 0.625rem 3rem;
+  color: ${theme.colors.main[5]};
+  outline: 0.0625rem solid ${theme.colors.gray[2]};
+
+  ::placeholder {
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: ${theme.colors.gray[6]};
+  }
+  &:hover {
+    outline: 0.0625rem solid ${theme.colors.main[4]};
+  }
+  &:focus {
+    outline: 0.0625rem solid ${theme.colors.main[5]};
+  }
+`;
+
+const ImageWrapper = styled.div<{ position?: string }>`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  top: 0;
+  bottom: 0;
+  left: ${({ position }) => position === "left" && "1.25rem"};
+  right: ${({ position }) => position === "right" && "1.25rem"};
+`;
+
+const ClearIconWrapper = styled.div`
+  position: relative;
+  padding: 0.25rem;
+  border-radius: 6.25rem;
+  background-color: #848c93;
+`;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,11 +1,21 @@
 import styled from "@emotion/styled";
-import { Navbar } from "../components";
+import { Navbar, Text, SearchBar } from "components";
+import { useGetWeatherData } from "hooks";
 
 export const Main = () => {
+  const { temperature, condition } = useGetWeatherData();
+  console.log(temperature);
+  console.log(condition);
   return (
     <>
       <Container>
         <Navbar />
+        <Wrapper>
+          <Text size={32} weight={600}>
+            오늘은 뭐먹지?
+          </Text>
+        </Wrapper>
+        <SearchBar />
       </Container>
     </>
   );
@@ -16,4 +26,9 @@ const Container = styled.div`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  margin: 11.25rem 0 2.75rem 0;
 `;
