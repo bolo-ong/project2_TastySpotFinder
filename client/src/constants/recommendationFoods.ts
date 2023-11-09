09a67d0723af6576e463d95648385473207a7b78
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-10 00:01:58 +0900

    [FEAT] create RecommendationFoods constant
    날씨, 기온별 추천 음식 객체 생성

---
+++ client/src/constants/recommendationFoods.ts
@@ -0,0 +1,11 @@
+export interface RecommendationFoods {
+  rain: string[];
+  [key: string]: string[];
+}
+export const recommendationFoods: RecommendationFoods = {
+  rain: ["김치전", "해물파전", "감자전", "파전", "삼겹살"],
+  0: ["떡만두국", "전골", "김치찌개"], // 0도 이하
+  10: ["순두부찌개", "짬뽕", "갈비탕", "잔치국수", "부대찌개", "된장찌개"], // 1도 ~ 10도
+  25: ["초밥", "햄버거", "피자", "불고기", "치킨"], // 11도 ~ 25도
+  100: ["삼계탕", "냉면", "빙수"], // 25도 이상
+};
