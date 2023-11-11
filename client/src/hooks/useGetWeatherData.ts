46dc4c51cc288ed2e6ab9b4233fef38d8b502dbc
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-11 17:38:02 +0900

    [FEAT] update useGetWeatherData
    기본값을 null로 지정

--- client/src/hooks/useGetWeatherData.ts
+++ client/src/hooks/useGetWeatherData.ts
@@ -3,19 +3,19 @@ import { getWeatherData } from "apis/weatherAPI";
 import { getLocation } from "utils";
 
 export const useGetWeatherData = () => {
-  const [temperature, setTemperature] = useState<string>();
-  const [condition, setCondition] = useState<string>();
+  const [temperature, setTemperature] = useState<string | null>(null);
+  const [condition, setCondition] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { latitude, longitude } = await getLocation();
 
-        const { tempCelsius, condition } = await getWeatherData(
+        const { temperature, condition } = await getWeatherData(
           latitude,
           longitude
         );
-        setTemperature(tempCelsius);
+        setTemperature(temperature);
         setCondition(condition);
       } catch (err) {
         console.error("날씨 정보를 가져오는 중 오류 발생:", err);
