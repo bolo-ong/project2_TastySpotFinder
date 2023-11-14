b445f48ef9d43d7784a4ca97239fe80b0b4b93a9
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-14 23:19:10 +0900

    [FEAT] 맛집 모델 업데이트
    맛집 리스트 모델과 관계를 형성

--- server/src/models/Restaurant.ts
+++ server/src/models/Restaurant.ts
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
 interface RestaurantType {
   name: string;
@@ -8,17 +8,37 @@ interface RestaurantType {
   menu: string[];
   img: string[];
   count?: number;
+  like?: number;
+  comments?: Comment[];
+  restaurantLists?: Types.ObjectId[];
 }
 
-const restaurantSchema = new Schema<RestaurantType>({
-  name: { type: String, required: true, unique: true, index: true },
-  category: { type: String },
-  location: { type: String },
-  contact: { type: String },
-  menu: [{ type: String }],
-  img: [{ type: String }],
-  count: { type: Number, default: 1 },
-});
+const restaurantSchema = new Schema<RestaurantType>(
+  {
+    name: { type: String, required: true, unique: true, index: true },
+    category: { type: String },
+    location: { type: String },
+    contact: { type: String },
+    menu: [{ type: String }],
+    img: [{ type: String }],
+    count: { type: Number, default: 1 },
+    like: { type: Number, default: 0 },
+    restaurantLists: [{ type: Schema.Types.ObjectId, ref: "RestaurantList" }],
+
+    comments: [
+      {
+        user: { type: String },
+        text: { type: String },
+      },
+      {
+        timestamps: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Restaurant = model<RestaurantType>("Restaurant", restaurantSchema);
 
