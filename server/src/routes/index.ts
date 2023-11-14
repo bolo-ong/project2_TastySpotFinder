d6d99f1e6b1e250e837c1741656f51418c176072
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-14 23:19:24 +0900

    [CHORE] 불필요한 코드 삭제

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,11 +4,6 @@ import restaurantRouter from "./restaurantRoutes";
 
 const router = express.Router();
 
-// const routineRouter = require("./routineRoutes");
-// const userRouter = require("./userRoutes");
-
-// router.use("/routine", routineRouter);
-// router.use("/user", userRouter);
 router.use("/auth", authRouter);
 router.use("/restaurant", restaurantRouter);
 
