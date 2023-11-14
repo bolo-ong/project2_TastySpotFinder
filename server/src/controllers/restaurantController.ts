0074cf9ceb11303d25c3f049ae1c299937dc54aa
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-14 23:18:02 +0900

    [FEAT] 맛집 추천 post응답 추가
    맛집 리스트를 먼저 추가하고, 프론트에 다시 응답을 보낸 후 해당 리스트의 id를 다시 받아서 크롤링 시작

---
+++ server/src/controllers/restaurantController.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import { crawlData } from "../utils/crawler";
+import {
+  Restaurant,
+  RestaurantType,
+  RestaurantList,
+  RestaurantListType,
+} from "../models";
+
+export const postRestaurantList = async (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+
+    const newRestaurantList: RestaurantListType = {
+      title: title,
+      description: description,
+      like: 0,
+      comments: [],
+    };
+
+    const createdRestaurantList = await RestaurantList.create(
+      newRestaurantList
+    );
+    const restaurantListId = createdRestaurantList._id;
+
+    res.send(restaurantListId);
+  } catch (err) {
+    res.status(500);
+  }
+};
+
+export const crawlRestaurant = async (req: Request, res: Response) => {
+  try {
+    const { crawlURL, restaurantListId } = req.body;
+    const id = new mongoose.Types.ObjectId(restaurantListId);
+
+    //크롤링을 통해 맛집 데이터 수집 후 Restaurant DB에 추가 또는 업데이트
+    const restaurants: RestaurantType[] = await crawlData(crawlURL);
+    if (restaurants) {
+      const bulkOps = restaurants.map((restaurant) => ({
+        updateOne: {
+          filter: { name: restaurant.name },
+          update: {
+            $set: restaurant,
+            $inc: { count: 1 },
+            $push: { restaurantLists: id },
+          },
+          upsert: true,
+        },
+      }));
+
+      await Restaurant.bulkWrite(bulkOps, { ordered: false });
+
+      res.send("게시물 등록이 완료되었습니다.");
+    } else {
+      res.status(400);
+    }
+  } catch (err) {
+    res.status(500);
+  }
+};
+
+export const getRestaurant = async (req: Request, res: Response) => {};
