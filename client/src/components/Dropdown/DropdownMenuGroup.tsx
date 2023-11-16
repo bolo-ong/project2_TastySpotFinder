3a49eb37d421452c4993568d3b3a0ca716cebe42
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-16 23:03:09 +0900

    [FEAT] update DropdownMenuGroup
    z-index 추가

--- client/src/components/Dropdown/DropdownMenuGroup.tsx
+++ client/src/components/Dropdown/DropdownMenuGroup.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import React, { useRef } from "react";
 import { useEscape } from "hooks";
+import { theme } from "styles/theme";
 
 export const DropdownMenuGroup = ({
   children,
@@ -26,6 +27,7 @@ export interface Props {
 }
 
 const StyledUl = styled.ul`
+  z-index: 2;
   position: absolute;
   top: 2.5rem;
 
@@ -35,10 +37,10 @@ const StyledUl = styled.ul`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-
   text-align: left;
 
   padding: 1rem 1.25rem;
   border-radius: 0.5rem;
+  background-color: ${theme.colors.pureWhite};
   box-shadow: 0 0.125rem 0.625rem rgba(63, 71, 77, 0.25);
 `;
