3b505d50d76836210fa4f9f8e6db047ee5a0abc8
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-17 23:19:13 +0900

    [FEAT] create auth middleware

--- server/src/routes/restaurantRoutes.ts
+++ server/src/routes/restaurantRoutes.ts
@@ -4,10 +4,11 @@ import {
   getRestaurant,
   crawlRestaurant,
 } from "../controllers/restaurantController";
+import { isAuthenticated } from "../middlewares/auth";
 
 const restaurantRouter = express.Router();
 
-restaurantRouter.post("/", postRestaurantList);
+restaurantRouter.post("/", isAuthenticated, postRestaurantList);
 restaurantRouter.post("/crawl", crawlRestaurant);
 restaurantRouter.get("/", getRestaurant);
 
