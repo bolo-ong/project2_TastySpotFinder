ac4a7286aeff638a91bd42a00ccc0a2b9c47c657
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-18 22:18:48 +0900

    [FEAT] create LoginRedirectPage component
    로그인 이전 prevPath값을 로컬스토리지에 저장해놨던걸 이용해서 이동시켜주는 컴포넌트

---
+++ client/src/pages/LoginRedirectPage.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+/*
+로그인 이후 서버에서 redirect시켜주는 페이지로
+로그인 이전 페이지인 prevPath값으로 이동시켜줌
+ */
+
+export const LoginRedirectPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const prevPath = localStorage.getItem("prevPath");
+    navigate(prevPath || "/", { replace: true });
+    localStorage.removeItem("prevPath");
+  }, []);
+
+  return null;
+};
