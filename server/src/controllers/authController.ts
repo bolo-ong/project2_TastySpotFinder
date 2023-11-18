36ec2f2fd2a7cc67e65f2d0673e50da2b51f7dd2
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-18 22:17:42 +0900

    [FEAT] update authController
    비로그인 상태에서 getUserProfile요청을 하면 Login required라고 응답해줌

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,33 +3,56 @@ import { Request, Response } from "express";
 const CLIENT_URL = process.env.CLIENT_URL ?? "http://localhost:3000";
 
 export const authenticateUser = (req: Request, res: Response) => {
-  res.redirect(CLIENT_URL + "/redirect");
+  try {
+    res.redirect(CLIENT_URL + "/redirect");
+  } catch (err) {
+    res.status(500);
+  }
 };
 
 export const logOutUser = (req: Request, res: Response) => {
-  req.logOut((err: Error) => {
-    if (err) {
-      res.status(500).send({ message: err.message });
-    }
-    req.session.save(() => {
-      res.send("Log out successful.");
+  try {
+    req.logOut((err: Error) => {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      }
+      req.session.save(() => {
+        res.send("Log out successful.");
+      });
     });
-  });
+  } catch (err) {
+    res.status(500);
+  }
 };
 
-export const logOutKakao = async (req: Request, res: Response) => {
-  req.logOut((err: Error) => {
-    if (err) {
-      res.status(500).send({ message: err.message });
-    }
-    req.session.save(() => {
-      res.redirect(CLIENT_URL);
+export const logOutKakao = (req: Request, res: Response) => {
+  try {
+    req.logOut((err: Error) => {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      }
+      req.session.save(() => {
+        res.redirect(CLIENT_URL);
+      });
     });
-  });
+  } catch (err) {
+    res.status(500);
+  }
 };
 
-export const getUser = (req: Request, res: Response) => {
-  if (req.isAuthenticated()) {
-    res.send(req.user);
+export const getUserProfile = (req: Request, res: Response) => {
+  try {
+    if (req.isAuthenticated()) {
+      const userProfile = {
+        displayName: req.user.displayName,
+        profile_image: req.user.profile_image,
+        provider: req.user.provider,
+      };
+      res.send(userProfile);
+    } else {
+      res.send("Login required");
+    }
+  } catch (err) {
+    res.status(500);
   }
 };
