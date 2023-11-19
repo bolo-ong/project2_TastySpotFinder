9eb714ee053ebcc585945fa776c9747e3f3eafe2
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-19 20:59:05 +0900

    [FEAT] create useToast hook

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useEscape } from "./useEscape";
 export { useGetWeatherData } from "./useGetWeatherData";
 export { useInterval } from "./useInterval";
 export { useNaverMap } from "./useNaverMap";
+export { useToast } from "./useToast";
