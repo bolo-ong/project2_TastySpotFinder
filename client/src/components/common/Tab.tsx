3b85e68dac210d64fa4930ee2b7e1c7fa607aa79
Lim Chaemin <111353138+bolo-ong@users.noreply.github.com>
2023-11-28 23:31:37 +0900

    [FEAT] create Tab component

--- client/src/components/common/Tab.tsx
+++ client/src/components/common/Tab.tsx
@@ -1,64 +1,46 @@
 import styled from "@emotion/styled";
-import { useRecoilState } from "recoil";
-import { activeTabState } from "recoil/atoms";
 import { theme } from "styles/theme";
 
 export interface Props {
   items: { title: string }[];
+  selectedTab: string;
+  onClick: (title: string) => void;
 }
 
-export const Tab = ({ items, ...rest }: Props) => {
-  const [activeTab, setActiveTab] = useRecoilState(activeTabState);
-
-  const handleTabClick = (title: string) => {
-    setActiveTab({ ...activeTab, title });
-  };
-
+export const Tab = ({ items, selectedTab, onClick, ...rest }: Props) => {
   return (
-    <Wrapper>
-      <Container>
-        {items.map((item) => (
-          <TabItem
-            key={item.title}
-            onClick={() => handleTabClick(item.title)}
-            className={activeTab.title === item.title ? "active" : ""}
-          >
-            {item.title}
-          </TabItem>
-        ))}
-      </Container>
-    </Wrapper>
+    <StyledTab>
+      {items.map((item) => (
+        <TabItem
+          key={item.title}
+          onClick={() => onClick(item.title)}
+          selected={selectedTab === item.title}
+        >
+          {item.title}
+        </TabItem>
+      ))}
+    </StyledTab>
   );
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  width: 100%;
-  height: 3.25rem;
-
-  background-color: #f6f8f9;
-  border: 1px solid ${theme.colors.gray};
-`;
-
-const Container = styled.ul`
-  display: flex;
-  flex-direction: row;
-  width: 67.75rem;
-`;
-
-const TabItem = styled.li`
+const TabItem = styled.li<{ selected: boolean }>`
+  cursor: pointer;
   display: flex;
   align-items: center;
   padding: 0 0.5rem;
   font-weight: 600;
   height: 3.25rem;
 
-  &.active {
-    color: ${theme.colors.main[5]};
-    border-top: 1px solid ${theme.colors.gray};
+  ${({ selected }) =>
+    selected &&
+    `
+    color:  ${theme.colors.main[5]};
+    border-top: 1px solid ${theme.colors.gray[0]};
     border-bottom: 1px solid ${theme.colors.main[5]};
-  }
+    `}
+`;
+
+const StyledTab = styled.ul`
+  display: flex;
+  flex-direction: row;
 `;
